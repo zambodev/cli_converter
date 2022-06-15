508cb63735a7920be6db71646384f0b89f0356b7
Zambo-dev <thomasz01@outlook.it>
2022-06-15 17:24:40 +0200

    Cli parameters

--- src/main.c
+++ src/main.c
@@ -7,17 +7,19 @@
 
 int main(int argc, char **argv)
 {	
+	if(argc < 4) 
+	{
+		puts("ERROR: Missing parameters <number> <base> <new_base>");
+		return EXIT_FAILURE;	
+	}
+
 	num_t num = {0, 0, 0};
 	char output[STR_LEN];
 
 
-	num_init(&num, "-123.123", 10);
-	num_debug(&num);
+	num_init(&num, argv[1], strtol(argv[2], NULL, 10));
 
-	num_to_str(&num, output, 16);
-	printf("%s\n", output);
-
-	num_to_str(&num, output, 2);
+	num_to_str(&num, output, strtol(argv[3], NULL, 10));
 	printf("%s\n", output);
 
 	return EXIT_SUCCESS;
