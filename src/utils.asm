40da899629314aec0e44f1c8463f7096001b17b1
zambodev <thomasz01@outlook.it>
2023-08-02 10:54:53 +0200

    strnorm

--- src/utils.asm
+++ src/utils.asm
@@ -1,20 +1,49 @@
 	section		.text
-	global		strcheck
+	global		strcheck, strnorm
+
+; rax = result
+; rdi = val
+strnorm:
+.loop:	cmp	byte [rdi], 0
+	je	.exit
+	cmp	byte [rdi], 57
+	jle	.c48
+	cmp	byte [rdi], 90
+	jle 	.c65
+	cmp	byte [rdi], 122
+	jle	.c97
+.c48:	cmp	byte [rdi], 48
+	mov	rax, 48
+	jge	.sub
+.c65:	cmp	byte [rdi], 65
+	mov	rax, 55
+	jge	.sub
+.c97:	cmp	byte [rdi], 97
+	mov	rax, 87
+	jge	.sub
+	jmp	.err
+.sub:	sub	byte [rdi], al
+
+	inc 	rdi
+	jmp	.loop
+
+.err:	mov	rax, -1
+
+.exit:	ret
 
 ; rax = result
 ; rdi = val
 ; rsi = base
 strcheck:
 	xor	rax, rax
-	add	rsi, 48
-loop:	cmp	byte [rdi], 0
-	je	exit
+.loop:	cmp	byte [rdi], 0
+	je	.exit
 	
 	cmp	byte [rdi], sil
-	jge	bad
+	jge	.err
 
 	inc	rdi
-	jmp	loop
+	jmp	.loop
 
-bad:	sub	rax, 1
-exit:	ret
+.err:	sub	rax, 1
+.exit:	ret
